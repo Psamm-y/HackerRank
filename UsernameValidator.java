653b8e2921c47abedc7ae8a97ff628b1e51019c4
Psammy <ampadupsamuel1@gmail.com>
2025-03-02 23:36:06 +0000

    Update

--- UsernameValidator.java
+++ UsernameValidator.java
@@ -1,19 +1,21 @@
 package HackerRank;
 
+import java.lang.classfile.attribute.SyntheticAttribute;
 import java.util.Scanner;
 
 public class UsernameValidator {
    /* You are updating the username policy on your company's internal networking platform.
    According to the policy, a username is considered valid if all the following constraints are satisfied:
 
-    The username consists of 8 to 30 characters inclusive. If the username consists of less than 8 or greater than 30 characters,
+    The username consists of 8 to 30 characters inclusive.
+    If the username consists of less than 8 or greater than 30 characters,
      then it is an invalid username.
     The username can only contain alphanumeric characters and underscores (_).
      Alphanumeric characters describe the character set consisting of lowercase characters ,
      uppercase characters , and digits .
     The first character of the username must be an alphabetic character,
     i.e., either lowercase character  or uppercase character .*/
-    public boolean characters(String text){
+    public boolean NumberOfCharacters(String text){
         boolean booleanResult= false;
         if (text.length()<30 && text.length()>8){
             booleanResult= true;
@@ -26,10 +28,16 @@ public class UsernameValidator {
     public boolean alphanumeric(Integer value){
         return value instanceof Integer;
     }
+    public boolean alphabetic(String text){
+        return (text.startsWith("*") || text.startsWith(".")||text.startsWith("/")) ;
+    }
     public static void main (String[] args){
         UsernameValidator uv = new UsernameValidator();
-        System.out.println(uv.alphanumeric("Samuel"));
-        System.out.println(uv.alphanumeric(23));
+        System.out.println("Please input a name");
+       Scanner input = new Scanner(System.in);
+       String name = input.next().trim();
+       if(uv.NumberOfCharacters(name) && uv.alphanumeric(name) && )
+
 
     }
 }
